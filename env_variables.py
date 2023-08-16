7d51d2dd72b35013d297924d99dee3aba9c66490
yching83 <scooby7890@gmail.com>
2023-08-16 09:18:22 -0400

    env_variables.py
    The env_variables file is using dotenv.

--- env_variables.py
+++ env_variables.py
@@ -1,8 +1,8 @@
-import dotenv
+#import dotenv
 import os #provides ways to access the Operating System and allows us to read the environment variables
-from dotenv import load_dotenv
+#from dotenv import load_dotenv
 
-load_dotenv()
+#load_dotenv()
 
 snowflake_username = os.getenv("SNOWFLAKE_USER")
 snowflake_password = os.getenv("SNOWFLAKE_PASSWORD")
@@ -14,3 +14,9 @@ snowflake_role_nm = os.getenv("ROLE_FIELD")
 dev_aws_access_key = os.getenv("DEV_ACCESS_KEY")
 dev_aws_secret_key = os.getenv("DEV_SECRET_KEY")
 dev_arn = os.getenv("DEV_ARN")
+dev_dynamo_table = os.getenv("DEV_DYNAMO_TABLE")
+dev_s3_bucket = os.getenv("DEV_BUCKET_NAME")
+prod_aws_access_key_id = os.getenv("PROD_AWS_ACCESS_KEY_ID")
+prod_aws_secret_access_key = os.getenv("PROD_AWS_SECRET_ACCESS_KEY")
+prod_aws_session_token = os.getenv("PROD_AWS_SESSION_TOKEN")
+
